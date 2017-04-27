47ccc141429d7c1df0fb05dffdc640a96ef6576d
Unknown <quent0798@gmail.com>
2017-04-27 11:24:48 +0200

    Commentaire code php
    Mise en commentaire du code php pour continuer à travailler sur le visuel

--- cadrepro.php
+++ cadrepro.php
@@ -52,16 +52,16 @@
 					<td id="pub">
 					<?php
 				
-					$user="yoann";
+					$user="quentin";
 					$pass="root";
 					try {
 						$dbh = new PDO('mysql:host=10.25.221.203;dbname=tv_db', $user, $pass);
 						foreach($dbh->query('SELECT * from prof') as $row) {
-							print_r($row);
+							//print_r($row);
 						}
 						$dbh = null;
 					} catch (PDOException $e) {
-						print "Erreur !: " . $e->getMessage() . "<br/>";
+						//print "Erreur !: " . $e->getMessage() . "<br/>";
 						die();
 					}
 					?>
